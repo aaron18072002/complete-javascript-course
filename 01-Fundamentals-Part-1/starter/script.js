75a7b985447bdbf42ec0e29e842b4e51e447cf0d
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2025-03-02 17:59:17 +0700

    updated

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,48 @@
-let js = "amazing";
-if (js === "amazing") {
-  alert("Javasript is FUN!");
-}
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// if (js === "amazing") {
+//   alert("Javasript is FUN!");
+// }
+// console.log(40 + 8 + 23 - 10);
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Matilda";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let jonas_matilda = "JM";
+// let $function = () => console.log();
+
+// const PI = 3.1415;
+
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
+
+// let isJavascriptFun = true;
+// console.log(isJavascriptFun);
+
+// console.log(typeof true);
+// console.log(typeof isJavascriptFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+// isJavascriptFun = "YES!";
+// console.log(typeof isJavascriptFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+let age = 30;
+age = 31;
+console.log(age);
+
+const birthYear = 1991;
+
+// const job;
+
+var job = "programmer";
+job = "teacher";
