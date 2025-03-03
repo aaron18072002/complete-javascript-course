1a8a6223ad75c57acf30a70c4137ffbf237bb54b
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2025-03-03 13:08:21 +0700

    updated

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -36,13 +36,101 @@
 
 // console.log(typeof null);
 
-let age = 30;
-age = 31;
-console.log(age);
+// let age = 30;
+// age = 31;
+// console.log(age);
 
-const birthYear = 1991;
+// const birthYear = 1991;
 
 // const job;
 
-var job = "programmer";
-job = "teacher";
+// var job = "programmer";
+// job = "teacher";
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3); // 2 ** 3 = 2 * 2 * 2
+
+// const firstName = "Jonas";
+// const lastName = "Schmedtmann";
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5;
+// x += 10; // x = x + 10
+// x *= 4; // x = x * 4;
+// x++; // x = x + 1
+// x--; // x = x - 1
+// x--;
+// console.log(x);
+
+// console.log(ageJonas > ageJonas); // > < >= <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2018);
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - ageJonas > now - ageSarah);
+
+// let x, y;
+// x = y = 25 - 10 - 5;
+// console.log(x, y);
+
+// const averageAge = (ageJonas + ageSarah) / 2;
+// console.log(ageJonas, ageSarah, averageAge);
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const markHigherBMI = BMIMark > BMIJohn;
+
+// console.log(BMIMark, BMIJohn, markHigherBMI);
+
+// const firstName = "Jonas";
+// const job = "teacher";
+// const birthYear = 1991;
+// const year = 2037;
+
+// const jonas =
+//   "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+// console.log(jonas);
+
+// const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+// console.log(jonasNew);
+
+// console.log(`Just a regular string...`);
+
+// console.log("String with \nmultiples\nline");
+
+// console.log(`String with
+// multiples
+// lines`);
+
+const age = 21;
+if (age >= 18) {
+  console.log("Sarah can start driving license 🚗");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+let century;
+if (birthYear >= 2000) {
+  century = 21;
+} else {
+  century = 20;
+}
+
+console.log(century);
